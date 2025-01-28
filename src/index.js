69b33065ca80c95a4f5761e6d7e95a2ac932b5de
thenowl <183875071+thenowl@users.noreply.github.com>
2025-01-28 21:18:53 +0100

    Initialize project and write drop-down menu function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+
+const productsLink = document.querySelector('#productsLink');
+const aboutLink = document.querySelector('#aboutLink');
+const contactLink = document.querySelector('#contactLink');
+
+function toggleDropDownVisibility() {
+  this.nextElementSibling.classList.toggle('visible');
+}
+
+productsLink.addEventListener('click', toggleDropDownVisibility);
+aboutLink.addEventListener('click', toggleDropDownVisibility);
+contactLink.addEventListener('click', toggleDropDownVisibility);
